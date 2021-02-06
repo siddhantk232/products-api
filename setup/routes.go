ee80a476ca23abd39a52a5d5994c0ffcb35c09d6
siddhantk232 <siddhantk232@gmail.com>
2021-02-06 20:35:12 +0530

    feat: setup routes

--- setup/routes.go
+++ setup/routes.go
@@ -7,13 +7,41 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// /     GET    - get all products.
+// /{id} GET    - get one product.
+// /     POST   - create product.
+// /{id} UPDATE - update a product.
+// /{id} DELETE - delete a product.
+
+// SetupRoutes register for products-api
 func SetupRoutes(sm *mux.Router) {
-	r := sm.PathPrefix("/products/").Subrouter()
+	sm.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 
-	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-
-		fmt.Fprintln(rw, "Hello World!")
+		fmt.Fprintln(rw, "GET products")
 
 	}).Methods("GET")
 
+	sm.HandleFunc("/{id:[0-9]+}", func(rw http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintln(rw, "GET one product")
+
+	}).Methods("GET")
+
+	sm.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintln(rw, "create product")
+
+	}).Methods("POST")
+
+	sm.HandleFunc("/{id:[0-9]+}", func(rw http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintln(rw, "UPDATE product")
+
+	}).Methods("UPDATE")
+
+	sm.HandleFunc("/{id:[0-9]+}", func(rw http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintln(rw, "DELETE product")
+
+	}).Methods("DELETE")
 }
